ccf578ebd38f282e347bc16c46fd62ee27a1134d
mohamad reza mohamadi <mrezamohamadi2259@gmail.com>
2021-09-29 13:40:20 +0330

    + added models + added tables

--- shop/app/Models/User.php
+++ shop/app/Models/User.php
@@ -18,10 +18,40 @@ class User extends \TCG\Voyager\Models\User
      * @var string[]
      */
     protected $fillable = [
+
         'name',
         'email',
         'password',
+        'phone',
+        'postal_code',
+        'address'
     ];
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function discounts()
+    {
+        return $this->belongsToMany(Discount::class);
+    }
+
+    public function cart()
+    {
+        return $this->hasOne(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function productRate()
+    {
+        return $this->hasOne(ProductRate::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.
---
+++ shop/database/migrations/2021_09_29_092717_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\App\Models\Category::class);
+            $table->tinyText('en_title');
+            $table->tinyText('fa_title');
+            $table->text('description');
+            $table->integer('price');
+            $table->tinyInteger('discount');
+            $table->tinyInteger('rate');
+            $table->integer('view_counter');
+            $table->string('status');
+            $table->json('other_atts');
+            $table->string('primary_img');
+            $table->json('other_img');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
